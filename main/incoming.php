c0582b625ed98e1adc80e3d57e32944f939f5505
meer34 <tanveerarshad39@gmail.com>
2019-07-09 11:21:36 +0500

    completed receipt printing

--- main/incoming.php
+++ main/incoming.php
@@ -1,32 +1,48 @@
 <?php
 session_start();
 include('../connect.php');
-$a = $_POST['invoice'];
-$b = $_POST['product'];
-$c = $_POST['qty'];
-$w = $_POST['pt'];
-$discount = $_POST['discount'];
-$result = $db->prepare("SELECT * FROM products WHERE product_code= :userid");
-$result->bindParam(':userid', $b);
-$result->execute();
-for($i=0; $row = $result->fetch(); $i++){
-$price=$row['price'];
-$name=$row['product_name'];
-}
 
-//edit qty
-$sql = "UPDATE products 
-        SET qty=qty-?
-		WHERE product_code=?";
-$q = $db->prepare($sql);
-$q->execute(array($c,$b));
-$priceAfterDiscount=$price-$discount;
-$d=$priceAfterDiscount*$c;
-// query
-$sql = "INSERT INTO sales_order (invoice,product,qty,amount,name,price,discount) VALUES (:a,:b,:c,:d,:e,:f,:g)";
-$q = $db->prepare($sql);
-$q->execute(array(':a'=>$a,':b'=>$b,':c'=>$c,':d'=>$d,':e'=>$name,':f'=>$price,':g'=>$discount));
-header("location: sales.php?id=$w&invoice=$a");
+$a = $_POST['invoice'];
+$w = $_POST['pt'];
+
+$chk = $db->prepare("SELECT product_code FROM products where product_code = :product");
+$chk->bindParam(':product', $_POST['product']);
+$chk->execute();
+$newChk = $chk->fetch();
+
+// echo $newChk['product_code'];
+
+if($newChk['product_code'] == $_POST['product'])
+{
+    $b = $_POST['product'];
+    $c = $_POST['qty'];
+    $discount = $_POST['discount'];
+    $result = $db->prepare("SELECT * FROM products WHERE product_code= :userid");
+    $result->bindParam(':userid', $b);
+    $result->execute();
+    for($i=0; $row = $result->fetch(); $i++){
+    $price=$row['price'];
+    $name=$row['product_name'];
+    }
+
+    //edit qty
+    $sql = "UPDATE products 
+            SET qty=qty-?
+    		WHERE product_code=?";
+    $q = $db->prepare($sql);
+    $q->execute(array($c,$b));
+    $priceAfterDiscount=$price-$discount;
+    $d=$priceAfterDiscount*$c;
+    // query
+    $sql = "INSERT INTO sales_order (invoice,product,qty,amount,name,price,discount) VALUES (:a,:b,:c,:d,:e,:f,:g)";
+    $q = $db->prepare($sql);
+    $q->execute(array(':a'=>$a,':b'=>$b,':c'=>$c,':d'=>$d,':e'=>$name,':f'=>$price,':g'=>$discount));
+    header("location: sales.php?id=$w&invoice=$a");
+}
+else
+{
+    header("location: sales.php?id=$w&invoice=$a");
+}
 
 
 ?>
--- main/preview.php
+++ main/preview.php
@@ -1,15 +1,16 @@
-<link href="../style.css" media="screen" rel="stylesheet" type="text/css" />
+<link href="invoice.css" media="screen" rel="stylesheet" type="text/css" />
 <script language="javascript">
 function Clickheretoprint()
 { 
-  var disp_setting="toolbar=yes,location=no,directories=yes,menubar=yes,"; 
-      disp_setting+="scrollbars=yes,width=800, height=400, left=100, top=25"; 
-  var content_vlue = document.getElementById("content").innerHTML; 
+  var disp_setting="toolbar=true,location=no,directories=yes,menubar=yes,"; 
+      disp_setting+="scrollbars=no"; 
+  var content_vlue = document.getElementById("invoice-POS").innerHTML; 
   
   var docprint=window.open("","",disp_setting); 
    docprint.document.open(); 
-   docprint.document.write('</head><body onLoad="self.print()" style="width: 800px; font-size: 13px; font-family: arial;">');          
+   docprint.document.write('</head><body onLoad="self.print()" style="font-size: 13px; font-family: arial;">');          
    docprint.document.write(content_vlue); 
+    docprint.document.write('</body></html>');
    docprint.document.close(); 
    docprint.focus(); 
 }
@@ -38,11 +39,10 @@ $amount=$cash-$am;
 ?><br>
 <br>
 
-<div class="content" id="content">
+<!-- <div class="content" id="content">
 <div style="margin: 0 auto; padding: 20px; width: 700px; font-weight: normal;">
 	<div style="width: 100%; height: 190px;" >
 	<div style="width: 459px; float: left;">
-	<!--<img src="../initial.png" />-->
 	<h1>fb - Fashion Botique</h1><br>
 	Street Address<br>
 	Brgy<br>
@@ -225,6 +225,92 @@ $amount=$cash-$am;
 	
 <div style="text-align: right; margin-top: 13px;">Cashier : <?php echo $cashier ?></div>
 </div>
-</div>
+</div> -->
 
+
+<div id="invoice-POS">
+    
+    <center id="top">
+      <div class="logo">
+		  <img src="../logo.png" alt="logo" width="60px" height="60px"/>
+	  </div>
+      <div class="info"> 
+        <h2>Abiding Tech</h2>
+      </div><!--End Info-->
+    </center><!--End InvoiceTop-->
+    
+    <div id="mid">
+      <div class="info">
+        <h2>Contact Info</h2>
+        <p> 
+            Address : Satellite Town, Green Center, Gujranwala </br>
+            Email   : support@abidingtech.com</br>
+            Phone   : 03064798395</br>
+        </p>
+      </div>
+    </div><!--End Invoice Mid-->
+    
+    <div id="bot">
+
+					<div id="table">
+						<table>
+							<tr class="tabletitle" style="background-color: #EEE;">
+								<td class="item" style="font-size: .5em;"><h2>Item</h2></td>
+								<td class="Hours" style="font-size: .5em;"><h2>Qty</h2></td>
+								<td class="Rate" style="font-size: .5em;"><h2>Sub Total</h2></td>
+							</tr>
+
+							<tr class="service">
+								<td class="tableitem"><p class="itemtext">Communication</p></td>
+								<td class="tableitem"><p class="itemtext">5</p></td>
+								<td class="tableitem"><p class="itemtext">375.00</p></td>
+							</tr>
+
+							<tr class="service">
+								<td class="tableitem"><p class="itemtext">Asset Gathering</p></td>
+								<td class="tableitem"><p class="itemtext">3</p></td>
+								<td class="tableitem"><p class="itemtext">225.00</p></td>
+							</tr>
+
+							<tr class="service">
+								<td class="tableitem"><p class="itemtext">Design Development</p></td>
+								<td class="tableitem"><p class="itemtext">5</p></td>
+								<td class="tableitem"><p class="itemtext">375.00</p></td>
+							</tr>
+
+							<tr class="service">
+								<td class="tableitem"><p class="itemtext">Animation</p></td>
+								<td class="tableitem"><p class="itemtext">20</p></td>
+								<td class="tableitem"><p class="itemtext">1500.00</p></td>
+							</tr>
+
+							<tr class="service">
+								<td class="tableitem"><p class="itemtext">Animation Revisions</p></td>
+								<td class="tableitem"><p class="itemtext">10</p></td>
+								<td class="tableitem"><p class="itemtext">750.00</p></td>
+							</tr>
+
+
+							<tr class="tabletitle" style="background-color: #EEE;">
+								<td></td>
+								<td class="Rate" style="font-size: .5em;"><h2>tax</h2></td>
+								<td class="payment" style="font-size: .5em;"><h2>419.25</h2></td>
+							</tr>
+
+							<tr class="tabletitle" style="background-color: #EEE;">
+								<td></td>
+								<td class="Rate" style="font-size: .5em;"><h2>Total</h2></td>
+								<td class="payment" style="font-size: .5em;"><h2>3,644.25</h2></td>
+							</tr>
+
+						</table>
+					</div><!--End Table-->
+
+					<div id="legalcopy">
+						<p class="legal"><strong>Thank you for your business!</strong>  Payment is expected within 31 days; please process this invoice within that time. There will be a 5% interest charge per month on late invoices. 
+						</p>
+					</div>
+
+				</div><!--End InvoiceBot-->
+  </div><!--End Invoice-->
 
